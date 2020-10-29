ab00e001a8b19e8eee4d11e1ebf4ea7029d48d94
jiangtao <a30389@gmail.com>
2020-10-29 19:17:08 +0800

    fix: 更新博客名称

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Pacino's Blog`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Pacino Jiang`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
